dc457ee125206b3407b8cdd387b61a207ed05bb9
SydZero <63630743+SydZero@users.noreply.github.com>
2023-04-02 23:01:08 -0700

    get_weather() function defined

---
+++ weather_api_requests.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_weather():
+    API_KEY = '1f451e8cde2c7cb6736f47733475b9e3'
+    location = 'College Station'
+    response = requests.get(f'http://api.weatherstack.com/current?access_key={API_KEY}&query={location}')
+    if response.status_code == 200:
+        weather_data = response.json()
+        temperature = weather_data['current']['temperature']
+        weather_description = weather_data['current']['weather_descriptions'][0]
+        print(f'The temperature in {location} is {temperature} degrees Celsius and the weather is {weather_description}.')
+    else:
+        print('Failed to get weather data.')
+
+
