781a27ef7fd4a070bd023fb3e75f3ae2a3a2f145
Emmie(Mengting) Teng <Mengting.teng@gmail.com>
2023-04-04 12:43:39 -0500

    implemented tables

--- spin/src/manager-view/Display.jsx
+++ spin/src/manager-view/Display.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import fakeData from './MOCK_DATA.json'
+import {useTable} from 'react-table'
+import Button from './Button';
 import './Display.css';
 
 
@@ -29,8 +32,67 @@ export default function Display(props) {
 }
 
 function Inventory(){
+    const data = React.useMemo(() => fakeData, [])
+    const columns = React.useMemo(
+        () => [{
+            Header: "ID",
+            accessor: "id",
+        }, {
+            Header: "First Name",
+            accessor: "first_name",
+        }, {
+            Header: "Last Name",
+            accessor: "last_name", 
+        }, {
+            Header: "Email",
+            accessor: "email",
+        }, {
+            Header: "Gender",
+            accessor: "gender",
+        }, {
+            Header: "University",
+            accessor: "university"
+        }, ], 
+        []
+    );
+
+    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data});
+
     return(
-        <h1>Inventory</h1>
+        <div>
+            <h1>Inventory</h1>
+            <div className='container'>
+                <table {...getTableProps()}>
+                <thead>
+                    {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                        <th {...column.getHeaderProps()}>
+                            {column.render("Header")}
+                        </th>
+                        ))}
+                    </tr>
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                            <td {...cell.getCellProps()}> {cell.render("Cell")} </td>
+                        ))}
+                        </tr>
+                    );
+                    })}
+                </tbody>
+                </table>
+            </div>
+            <button className="inven">Restock</button>
+        </div>
+
+        
+
     )
 }
 
