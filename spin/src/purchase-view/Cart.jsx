60c825a5a86737c4b41f0081e820e3ba51a5d47d
Emmie(Mengting) Teng <Mengting.teng@gmail.com>
2023-04-04 21:40:46 -0500

    implemented graphical components for cart

--- spin/src/purchase-view/Cart.jsx
+++ spin/src/purchase-view/Cart.jsx
@@ -6,6 +6,33 @@ export default function Cart() {
     return (
         <div className="cart-box">
             <h1>Cart</h1>
+
+            <OrderList/>
+
+            <div className='button-container'>
+                <AddButton/>
+                <CheckoutButton/>
+            </div>
+        </div>
+    );
+}
+
+function AddButton(){
+    return(
+        <button className='button-cart'>Add</button>
+    );
+}
+
+function CheckoutButton(){
+    return(
+        <button className='button-cart'>Checkout</button>
+    );
+}
+
+function OrderList(){
+    return(
+        <div className='orders-container'>
+            <p>test</p>
         </div>
     );
 }
