3e2db280284b6df860c062129206742957cf0d43
Emmie(Mengting) Teng <Mengting.teng@gmail.com>
2023-04-05 09:52:49 -0500

    implementation of weather api call

--- spin/src/Navbar.jsx
+++ spin/src/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import useSWR from 'swr';
+import axios from 'axios';
+
+const fetcher = (url) => axios.get(url).then(res => res.data);
 
 /* navbar includes login */
 export default function Navbar(props) {
@@ -12,7 +16,27 @@ export default function Navbar(props) {
             <div className="left-nav">
                 <h1>{props.displayText}</h1>
             </div>
+            <WeatherAPIDisplay/>
             <button className="login-btn" onClick={handleClick}>{props.buttonText}</button>
         </nav>
     );
+}
+
+function WeatherAPIDisplay(){
+    const { data, error, isLoading } = useSWR('http://localhost:5000/weather', fetcher);
+    if (error) {
+      console.error(error);
+    }
+
+    if(isLoading)
+        return;
+
+    var weather = JSON.parse(data);
+    console.log(weather)
+
+    var text = 'Temperature at ' + weather['location']['name'] + ", " + weather['location']['region'] + 
+               ' is ' + weather['current']['temperature'] + '°C, ' + weather['current']['weather_descriptions'][0]
+    return(
+        <h2>{text}</h2>
+    )
 }
