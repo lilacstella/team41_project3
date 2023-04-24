da1af82af1095a13824d6b7ddfee3756ed126bb1
Hunter Zacha <hunterzacha@tamu.edu>
2023-04-24 10:05:41 -0500

    get request done

--- stone/prices.py
+++ stone/prices.py
@@ -38,6 +38,14 @@ def get_prices():
             storagelist.append(row[0])
         prices_dict["storage"] = storagelist
 
+        get_no_img = """select distinct t.item from 
+        (select inventoryitem as item from inventory_t where category IN ('Topping-Meat', 'Topping-Veggies', 'Seasonal', 'Sauce', 'Drizzle', 'Drink', 'Dough', 'Cheese') and inventoryitem not in ('Regular Dough')
+        ) t left join item_images i on t.item = i.item_name
+        where i.item_name is NULL"""
+        cursor.execute(get_no_img)
+        no_img_items = [row[0] for row in cursor.fetchall()]
+        prices_dict["no-img"] = no_img_items
+
         return prices_dict
     
     finally:
@@ -98,6 +106,26 @@ def change_price(json_file):
         change_price = "UPDATE menu_t SET price = %s WHERE menuitem = %s"
         price_tuple = (json_file["price"], json_file["menuitem"])
         cursor.execute(change_price, price_tuple)
+        connection.commit()
+        return True
+    
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+
+def add_img(json_file):
+    connection = None
+    try:
+        connection = psycopg2.connect(user="csce315331_team_41_master",
+                                      password="goldfishwithnuts",
+                                      host="csce-315-db.engr.tamu.edu",
+                                      database="csce315331_team_41")
+        cursor = connection.cursor()
+        add_img = "INSERT INTO item_images (%s, %s)"
+        img_tuple = (json_file["item-name"], json_file["img-link"])
+        cursor.execute(add_img, img_tuple)
         connection.commit()
         return True
     
