8286401916825503b344e53f793d80dd77fd73c3
Emmie(Mengting) Teng <Mengting.teng@gmail.com>
2023-04-26 09:49:28 -0500

    set up server view

---
+++ spin/src/server_view/Gallery.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import useSWR from 'swr';
+import axios from 'axios';
+import './Gallery.css';
+import { HOST } from '..';
+
+
+const fetcher = (url) => axios.get(HOST + url).then(res => res.data);
+
+// maintaining aspect ratio of the image
+const AspectRatio = (props) => {
+    const { ratio = 1 / 1, ...otherProps } = props;
+
+    return (
+        <div
+            className="aspect-ratio"
+            style={{ paddingTop: `${100 / ratio}%` }}
+            {...otherProps}
+        />
+    );
+};
+
+const Image = (props) => {
+    // using props for url in the future
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${props.image})`,
+                backgroundSize: "cover",
+            }}
+        />
+    );
+};
+
+const ItemBox = (props) => {
+    // highlight item if it is in the order
+    const {itemName, image, order, addToOrder, pizza} = props;
+    const select = () => {
+        addToOrder(itemName);
+    }
+
+    let classes = "menu-item-box"
+    if (order.includes(itemName) || pizza['topping'].includes(itemName) || Object.values(pizza).some(val => val === itemName))
+        classes += " selected";
+    
+    return (
+        <figure className={classes} onClick={select}>
+            <AspectRatio>
+                <Image image={image} />
+            </AspectRatio>
+            <figcaption className="menu-item-box-caption">
+                {itemName}
+            </figcaption>
+        </figure>
+    );
+};
+
+const ItemBoxes = (props) => {
+    const { itemNames, category, order, addToOrder, pizza} = props;
+
+    return (
+        <div className="menu-item-box-frame">
+            {
+                // itemName is Object{"category-name": }
+                itemNames.map((itemName) => (
+                    <ItemBox itemName={itemName[category + "-name"]} image={itemName.image} order={order} addToOrder={addToOrder} pizza={pizza}/>
+                ))
+            }
+        </div>
+    );
+};
+
+const Grid = (props) => {
+    return <div className="grid" {...props} />;
+};
+
+// can be changed to be list view or grid view
+function MenuItems(props) {
+    const { itemNames, category, order, addToOrder, pizza } = props;
+
+    return (
+        <div className="gallery">
+            <Grid>
+                <ItemBoxes itemNames={itemNames} category={category} order={order} addToOrder={addToOrder} pizza={pizza}/>
+            </Grid>
+        </div>
+    );
+}
+
+export default function MenuGallery(props) {
+    // sauce, topping, cheese, drizzle, drink, dough, seasonal
+    const { view, order, addToOrder, pizza } = props;
+    const { data, error, isLoading } = useSWR('menu', fetcher);
+    if (error || isLoading)
+        return;
+
+    // console.log(data);
+    return (
+        <div className="order-box">
+            <MenuItems itemNames={data[view]} category={view} order={order} addToOrder={addToOrder} pizza={pizza}/>
+        </div>
+    );
+};
+
+export function OrderHistory() {
+    return (
+        <div>
+            hi
+        </div>
+    );
+}
---
+++ spin/src/server_view/ServerView.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import MenuGallery, {OrderHistory} from './Gallery';
+import axios from 'axios';
+import Cart from '../purchase_view/Cart';
+import './ServerView.css';
+import { HOST } from '..';
+
+export default function ServerView(props) {
+    const [currView, setCurrView] = useState('sauce');
+    const [pizza, setPizza] = useState({'topping': []});
+    const [order, setOrder] = useState([]);
+
+    const addToOrder = (item) => {
+        // console.log(item + " " + currView);
+        if (['sauce', 'cheese', 'dough', 'topping', 'drizzle'].includes(currView)) {
+            if (currView === 'topping') {
+                if (pizza['topping'].length >= 4)
+                    return;
+                else
+                    setPizza({...pizza, 'topping' : [...pizza['topping'], item]});
+            }
+            else
+                setPizza({ ...pizza, [currView]: item });
+        } else {
+            setOrder([...order, item]);
+        }
+        // console.log(order);
+        // console.log(pizza);
+    }
+
+    const addPizzaToOrder = () => {
+        setOrder([...order, pizza]);
+        setPizza({'topping': []});
+    }
+
+    const checkoutOrder = () => {
+        // formatting order
+        // console.log(order);
+        /* 
+        {
+            "order": ["Gatorade", {"sauce": "Zesty Red", "drizzle": "Ranch"}],
+            "employee_id": 1,
+            "payment_form": "cash",
+        }
+
+        */
+        axios.post(HOST + 'menu', {"payment_form": "cash", "employee_id": 0, "order": order});
+        setOrder([]);
+        alert('Order placed!');
+    }
+
+    const clearOrder = () => {
+        setOrder([]);
+        setPizza({'topping': []});
+
+        alert('Order cleared!');
+    }
+
+    return (
+        <div className="server-frame">
+            <Navigation handleClick={setCurrView} setMenuView={props.setMenuView}/>
+            {currView === 'order history' ? <OrderHistory /> : <MenuGallery view={currView} order={order} addToOrder={addToOrder} pizza={pizza}/>}
+            <Cart order={order} pizza={pizza} add={addPizzaToOrder} checkout={checkoutOrder} clear={clearOrder} setOrder={setOrder} setPizza={setPizza}/>
+        </div>
+    )
+}
+
+// can be hidden in server view
+function Navigation(props) {
+    // handling the click for each tab
+    return (
+        <div className="server-view-tab-frame">
+            <Tab name="Sauce" switchTab={props.handleClick} />
+            <Tab name="Cheese" switchTab={props.handleClick} />
+            <Tab name="Topping" switchTab={props.handleClick} />
+            <Tab name="Drizzle" switchTab={props.handleClick} />
+            <Tab name="Drink" switchTab={props.handleClick} />
+            <Tab name="Dough" switchTab={props.handleClick} />
+            <Tab name="Seasonal" switchTab={props.handleClick} />
+            <Tab name="Order History" switchTab={props.handleClick} />
+            <Tab name="Menu View" switchTab={props.setMenuView} />
+        </div>
+    );
+}
+
+function Tab(props) {
+    return (
+        <div className="server-view-tab" style={{ backgroundColor: props.color }} onClick={() => props.switchTab(props.name.toLowerCase())}>
+            <h2>{props.name}</h2>
+        </div>
+    );
+}
