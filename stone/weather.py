ab39dc1a31363d56a911d93e4b2da82ccde992f0
ana-jimenez <90536248+ana-jimenez@users.noreply.github.com>
2023-05-01 03:18:42 -0500

    comments for weather.py

--- stone/weather.py
+++ stone/weather.py
@@ -4,9 +4,36 @@ import functools
 
 from stone import WEATHER_API_KEY
 
+"""
+This module defines a function to retrieve the current weather data of a specified location using the Weatherstack API.
+
+Dependencies:
+- requests
+- json
+- functools
+- stone module containing the Weatherstack API key (WEATHER_API_KEY)
+
+Functions:
+- get_weather()
+
+Example Usage:
+
+mydatabase.get_weather()
+The temperature in College Station is 75 degrees Farenheit and the weather is Partly cloudy.
+"""
 
 @functools.cache
 def get_weather():
+    """
+    Retrieves the current weather data for a specified location using the Weatherstack API. 
+    Caches the response using functools.cache.
+
+    Args:
+    - None
+
+    Returns:
+    - A JSON string containing the weather data for College Station.
+    """
     location = 'College Station'
     response = requests.get(
         f'http://api.weatherstack.com/current?access_key={WEATHER_API_KEY}&query={location}&units=f')
