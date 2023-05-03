215ac924124b6c1db4bf0a61dda8f946bc1570e5
Emmie(Mengting) Teng <Mengting.teng@gmail.com>
2023-05-03 17:54:03 -0500

    change prod host for https and use SSL certificate

--- spin/src/index.js
+++ spin/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-export const HOST = 'http://pizzapos.sytes.net:5000/';
+export const HOST = 'https://pizzapos.sytes.net:5000/';
 export const G_CLIENT_ID = '257525904215-rj4iugd6avdihsnv7p0iuadsv8c91lgj.apps.googleusercontent.com';
 
 // Add a script tag to the document to load the Google Translate API
